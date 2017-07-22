f977a0bd687da4c7ef4e9779225e5578d9c69711
Mostavenco <guimostavenco@gmail.com>
2017-07-21 21:28:45 -0300

    modificada migration padrao do users e feita a migration products

--- database/migrations/2017_07_22_002423_create_products_table.php
+++ database/migrations/2017_07_22_002423_create_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -15,12 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('cpf')->unique();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('endereco');
-            $table->string('telefone');
+            $table->string('preco');
+            $table->string('codigo_de_barras')->unique();
+            $table->string('imagem');
+            $table->string('quantidade');
+            $table->string('nome');
+            $table->string('categoria');
         });
     }
 
@@ -31,6 +31,7 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
+        //
         Schema::dropIfExists('users');
     }
 }
